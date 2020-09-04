8558ca5ab586bc4a563530e4a00348f1a3109e25
Shayan <Shayan@DESKTOP-BU8G87T>
2020-09-05 01:03:24 +0530

    FirstCommint

---
+++ AssestComplaint/src/main/java/com/rafi/sakib/AssestComplaint/AssestComplaintApplication.java
@@ -0,0 +1,13 @@
+package com.rafi.sakib.AssestComplaint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssestComplaintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssestComplaintApplication.class, args);
+	}
+
+}
---
+++ AssestComplaint/src/test/java/com/rafi/sakib/AssestComplaint/AssestComplaintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafi.sakib.AssestComplaint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssestComplaintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
